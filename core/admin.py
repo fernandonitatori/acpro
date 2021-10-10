1bc06d6e730283ef0dfd96e9ec13375a664dc5d5
pi <fernando.nitatori@unesp.br>
2021-10-10 10:29:05 -0300

    Acrescentando projeto acpro e o app core (núcleo) do sistema

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Fornecedor, Parametro
 
-# Register your models here.
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cnpj', 'site', 'observacoes', ]
+
+@admin.register(Parametro)
+class ParametroAdmin(admin.ModelAdmin):
+    list_display = ['descricao', 'valor', 'observacoes', ]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.db.models import signals
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Base(models.Model):
+    criado = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Data de criação', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Fornecedor(Base):
+    nome = models.CharField('Nome', max_length=50)
+    cnpj = models.CharField('CNPJ', max_length=50)
+    site = models.CharField('Site', max_length=100)
+    observacoes = models.CharField('Observaçoes', max_length=100)
+
+    class Meta:
+        verbose_name = 'Fornecedor'
+        verbose_name_plural = 'Fornecedores'
+
+    def __str__(self):
+        return self.nome
+
+class Parametro(Base):
+    descricao = models.CharField('Descrição', max_length=50)
+    valor = models.CharField('Valor', max_length=50)
+    observacoes = models.CharField('Observaçoes', max_length=100)
+
+    class Meta:
+        verbose_name = 'Parâmetro'
+        verbose_name_plural = 'Parâmetros'
+
+    def __str__(self):
+        return self.descricao
