de11c13dc475430d7b715176c0b6f7bd885e1d15
fernandonitatori <fernando.nitatori@unesp.br>
2022-06-04 21:18:37 -0300

    Alterações na tela inicial do sistema

--- acpro/settings.py
+++ acpro/settings.py
@@ -89,11 +89,12 @@ WSGI_APPLICATION = 'acpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-
+"""
 #Para usar no Heroku
 DATABASES = {
     'default': dj_database_url.config()
 }
+"""
 
 """
 DATABASES = {
@@ -108,7 +109,7 @@ DATABASES = {
 }
 """
 
-"""
+
 #Postgres
 DATABASES = {
     'default': {
@@ -120,7 +121,7 @@ DATABASES = {
         'PORT:': '5432',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
