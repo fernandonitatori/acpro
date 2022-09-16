b751e7cd16d4218a17add73ebf4c9dc4ae55979c
Pi2-univesp2022 <106114549+Pi2-univesp2022@users.noreply.github.com>
2022-09-16 13:33:39 -0300

    Teste 2

--- acpro/settings.py
+++ acpro/settings.py
@@ -179,8 +179,8 @@ DATETIME_FORMAT = ('d/m/Y H:i')
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-  #      'rest_framework.authentication.SessionAuthentication'
-         'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.SessionAuthentication'
+     #    'rest_framework.authentication.TokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
